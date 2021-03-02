1c920a25b5a3a9e7a41ca93fcd20ecc239d5b94c
Артём <dradinc@yandex.ru>
2021-03-02 14:54:02 +0300

    create main.py

---
+++ Тесты кода/Python/main.py
@@ -0,0 +1,10 @@
+# Импорт библиотек
+import flask, flask_restful # Для создания "сервера"
+
+# Переменные
+server = Flask(__name__)
+
+# Что-то там
+
+if __name__ == '__main__':
+    server.run(debag=True)
