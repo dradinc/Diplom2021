b94942b8f0389ccbce822ae35dd72016e19b9aa0
Unknown <unknown@example.com>
2021-03-02 11:25:26 +0300

    edit

--- Тесты кода/Python/test2.py
+++ Тесты кода/Python/test2.py
@@ -4,15 +4,15 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
-con = pymysql.connect(host='127.0.0.1', user='root', password='root', db='workshops_ptk')
+#con = pymysql.connect(host='127.0.0.1', user='root', password='root', db='workshops_ptk')
 
 @app.route('/auth/login', methods=['POST'])
 def auth_login():
     parameters = request.get_json()
     return parameters
 
-    with con:
-        cur = con.cursor()
+    #with con:
+    #    cur = con.cursor()
 
 # Старт "сервера"
 if __name__ == '__main__':
