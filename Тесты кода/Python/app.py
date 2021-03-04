44c12c29671fb322aacb6a73f20011290a3caf50
Артём <dradinc@yandex.ru>
2021-03-04 16:09:19 +0300

    Create authorization

--- Тесты кода/Python/app.py
+++ Тесты кода/Python/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, redirect, render_template, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 import configparser
+import hashlib
 
 '''
 Импортированные библиотеки:
@@ -15,8 +16,10 @@ import configparser
 
 # Переменные приложения и их настройки
 app = Flask(__name__)  # Создаём переменную приложенияя
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # Конфинупация приложения используемая для работы с базой данных
+app.secret_key = 'sercet_key'
+app.config['SESSION_TYPE'] = 'o da'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # Конфинупация приложения используемая для работы с базой данных
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 appDataBase = SQLAlchemy(app)  # Переменная для взаимодействия с базой данных
 config = configparser.ConfigParser()  # Создаём переменную для работы с конфигурациями
 config.read('config.ini')  # Читаем данные конфигурации
@@ -25,11 +28,20 @@ config.read('config.ini')  # Читаем данные конфигурации
 # Классы приложения
 class userTable(appDataBase.Model):
     id = appDataBase.Column(appDataBase.Integer, primary_key=True)
-    login = appDataBase.Column(appDataBase.String, nullable=False)
-    password = appDataBase.Column(appDataBase.Binary, nullable=False)
+    login = appDataBase.Column(appDataBase.String(35), nullable=False)
+    password = appDataBase.Column(appDataBase.String(64), nullable=False)
 
     def __repr__(self):
         return '<userTable %r>' % self.id
+
+    def get_id(self):
+        return self.id
+
+    def get_login(self):
+        return self.login
+
+    def get_password(self):
+        return self.password
 
 
 @app.route('/')
@@ -41,8 +53,19 @@ def OpenSite():
 def AdminPage():
     if request.method == 'POST':
         login = request.form["login"]
-        password = request.form["password"]
-        return login + ' ' + password
+        password = hashlib.sha512(request.form["password"].encode('utf-8'))
+
+        try:
+            users = userTable.query.all()
+            for user in users:
+                if (user.get_login() == login) and (user.get_password() == password.hexdigest()):
+                    return 'Успешная авторизация'
+                    break
+                else:
+                    flash('Логин или пароль введены не верно!')
+                    return redirect('/admin')
+        except:
+            return 'Ошибка'
     elif request.method == 'GET':
         return render_template('admin.html')
 
